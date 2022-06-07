7c0eff16bb0815a0043c335328009600e2f4f5bc
Nusha34 <chechulinaan17@gmail.com>
2022-06-07 13:53:53 +0000

    load concept_name, id

--- src/automapping/concept.py
+++ src/automapping/concept.py
@@ -1,5 +1,7 @@
 from dataclasses import dataclass
-from typing import Sequence
+#from typing import Sequence
+#from typing import Iterable 
+#TODO change types
 
 
 @dataclass
@@ -8,7 +10,20 @@ class Concept:
     A concept in a medical ontology like SNOMED-CT.
     """
 
-    concept_id: int
-    names: Sequence[str]
+    #concept_id: int
+    #names: Sequence[str]
+        
+    def __init__(self, vocabulary_name, data):#, language: language.Language):
+        #super().__init__(language)
+        #self.path=path
+        self.vocabulary_name=vocabulary_name #TODO to upcase
+        self.data=data
+        
 
-    
+    def get_concept(self):
+        #TODO comments
+        data_local=self.data[(self.data['vocabulary_id']==self.vocabulary_name) & (self.data['standard_concept']=='S')]
+        data_local['concept_name']=data_local['concept_name'].replace({'X]': 'X] '}, regex=True)#TODO add exception
+        data_local=data_local[['concept_name','concept_id']]
+        return data_local
+
