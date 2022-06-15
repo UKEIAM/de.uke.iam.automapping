7da70e33205cc9a6bd7489535950b751ad2155ce
Nusha34 <chechulinaan17@gmail.com>
2022-06-15 09:26:31 +0000

    black formatting

--- src/automapping/loader.py
+++ src/automapping/loader.py
@@ -8,6 +8,7 @@ class Loader:
     """
     A class loading samples from a source.
     """
+
     def __init__(self, language: Language):
         self.language = language
 
@@ -24,12 +25,12 @@ class ExcelLoader(Loader):
 
     def __init__(self, path: str, column: str, language: Language):
         super().__init__(language)
-        self.path=path
-        self.column=column
-        self.data=pd.read_excel(self.path, usecols=[self.column])
-        
+        self.path = path
+        self.column = column
+        self.data = pd.read_excel(self.path, usecols=[self.column])
+
     def __iter__(self) -> Iterable[str]:
-        self.data=self.data.replace([' ', ''], np.nan)
-        self.data[self.column]=self.data[self.column].str.strip()
-        self.data=self.data[self.column].dropna(axis=0)
+        self.data = self.data.replace([" ", ""], np.nan)
+        self.data[self.column] = self.data[self.column].str.strip()
+        self.data = self.data[self.column].dropna(axis=0)
         return iter(self.data)
