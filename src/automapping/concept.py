d47a7985f5f041663017d0b3ca21555945a02c3a
Nusha34 <chechulinaan17@gmail.com>
2022-06-15 08:28:03 +0000

    reverse

--- src/automapping/concept.py
+++ src/automapping/concept.py
@@ -1,7 +1,5 @@
 from dataclasses import dataclass
-#from typing import Sequence
-#from typing import Iterable 
-#TODO change types
+
 
 
 @dataclass
@@ -13,17 +11,4 @@ class Concept:
     #concept_id: int
     #names: Sequence[str]
         
-    def __init__(self, vocabulary_name, data):#, language: language.Language):
-        #super().__init__(language)
-        #self.path=path
-        self.vocabulary_name=vocabulary_name #TODO to upcase
-        self.data=data
-        
-
-    def get_concept(self):
-        #TODO comments
-        data_local=self.data[(self.data['vocabulary_id']==self.vocabulary_name) & (self.data['standard_concept']=='S')]
-        data_local['concept_name']=data_local['concept_name'].replace({'X]': 'X] '}, regex=True)#TODO add exception
-        data_local=data_local[['concept_name','concept_id']]
-        return data_local
 
