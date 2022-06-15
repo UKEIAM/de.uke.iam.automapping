a19f2c230db984cad15259e319fa7db3c1584d07
Nusha34 <chechulinaan17@gmail.com>
2022-06-15 15:03:10 +0000

    try to satisfy pylint

--- src/automapping/translator.py
+++ src/automapping/translator.py
@@ -1,8 +1,8 @@
-from typing import Sequence
-from loader import Loader
-from language import Language
-from preprocessor import Preprocessor
 from transformers import pipeline
+from typing import Sequence 
+from . import Loader
+from . import Language
+from . import Preprocessor
 
 
 class Translator:
