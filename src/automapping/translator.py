8d10b1079bfc91243da62becf335e29114309345
Nusha34 <chechulinaan17@gmail.com>
2022-06-15 09:33:06 +0000

    translator black formatting

--- src/automapping/translator.py
+++ src/automapping/translator.py
@@ -1,5 +1,4 @@
 from typing import Sequence
-#from . import Language
 from loader import Loader
 from language import Language
 from preprocessor import Preprocessor
@@ -11,7 +10,7 @@ class Translator:
     A step in the pipeline doing the actual translation.
     """
 
-    def __init__(self, source_language: Language,  target_language: Language):
+    def __init__(self, source_language: Language, target_language: Language):
         self.target_language = target_language
         self.source_language = source_language
 
@@ -28,16 +27,18 @@ class HuggingFace(Translator):
     Use HiggingFace for translations.
 
     """
-    def __init__(self, source_language: Language,  target_language: Language):
+
+    def __init__(self, source_language: Language, target_language: Language):
         self.target_language = target_language
         self.source_language = source_language
-        self.model=f"Helsinki-NLP/opus-mt-{self.target_language.value}-{self.source_language.value}"
+        self.model = f"Helsinki-NLP/opus-mt-{self.target_language.value}-{self.source_language.value}"
 
-
-    def translate(self, data: Loader, preprocessor: Sequence[Preprocessor]) -> Sequence[str]:
-        tr_list=[]
-        translator=pipeline('translation', self.model)
-        samples=list(preprocessor(data))
+    def translate(
+        self, data: Loader, preprocessor: Sequence[Preprocessor]
+    ) -> Sequence[str]:
+        tr_list = []
+        translator = pipeline("translation", self.model)
+        samples = list(preprocessor(data))
         for sample in translator(samples):
-            tr_list.append(sample.get('translation_text'))
+            tr_list.append(sample.get("translation_text"))
         return tr_list
