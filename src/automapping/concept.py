cfd9cb0a4f0cd01dbb2dedf9ec01de654fac8c20
Nusha34 <chechulinaan17@gmail.com>
2022-06-22 10:51:04 +0000

    add concept

--- src/automapping/concept.py
+++ src/automapping/concept.py
@@ -1,5 +1,7 @@
 from dataclasses import dataclass
-from typing import Sequence
+import pandas as pd
+
+# from typing import Iterable, Sequence
 
 
 @dataclass
@@ -8,5 +10,20 @@ class Concept:
     A concept in a medical ontology like SNOMED-CT.
     """
 
-    concept_id: int
-    names: Sequence[str]
+    def __init__(self, path: str, vocabulary_id: str, standart_concept: str):
+        self.path = path
+        self.vocabulary_id = vocabulary_id
+        self.standart_concept = standart_concept
+        self.omop_concepts = pd.read_csv(
+            path, on_bad_lines="skip", delimiter="\t", low_memory=False
+        )
+
+    def __call__(self):
+        concepts = self.omop_concepts[
+            (self.omop_concepts["vocabulary_id"] == self.vocabulary_id)
+            & (self.omop_concepts["standard_concept"] == self.standart_concept)
+        ]
+        # concepts = concepts[['concept_id', 'concept_name']]
+        concepts_id = concepts["concept_id"].tolist()
+        concept_names = list(map(str, concepts["concept_name"]))
+        return concepts_id, concept_names
