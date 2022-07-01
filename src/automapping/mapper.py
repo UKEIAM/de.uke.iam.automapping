aa74eddc2903bbb64751fe62bc7e91a4d380756b
Nusha34 <chechulinaan17@gmail.com>
2022-07-01 10:28:34 +0000

    init

--- src/automapping/mapper.py
+++ src/automapping/mapper.py
@@ -1,7 +1,10 @@
 from dataclasses import dataclass
 from typing import Sequence
-
-from .concept import Concept
+from numpy import matrix
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+import heapq
+from concept import Concept
 
 
 @dataclass
@@ -10,7 +13,9 @@ class Prediction:
     A prediction with associated confidence score.
     """
 
+    term: str
     predicted_id: int
+    predicted_name: str
     confidence: float
 
 
@@ -35,7 +40,38 @@ class TfIdf(Mapper):
     """
 
     def __init__(self, concepts: Sequence[Concept]):
-        raise NotImplementedError()
+        self.concepts = concepts
 
     def __call__(self, data: str, num_guesses: int) -> Sequence[Prediction]:
-        raise NotImplementedError()
+        matrix_with_similarity_score = (
+            self.create_matrix_with_cosine_sim_between_term_concept(data)
+        )
+        terms = []
+        concept_ids = []
+        concept_names = []
+        sim_score = []
+        for i in range(matrix_with_similarity_score.shape[0]):
+            for seq_number, score in heapq.nlargest(
+                num_guesses,
+                enumerate(matrix_with_similarity_score[i]),
+                key=lambda x: x[1],
+            ):
+                terms.append(data[i])
+                concept_ids.append(self.concepts.concept_id[seq_number])
+                concept_names.append(self.concepts.names[seq_number])
+                sim_score.append(score)
+        return Prediction(
+            terms,
+            concept_ids,
+            concept_names,
+            sim_score,
+        )
+
+    def create_matrix_with_cosine_sim_between_term_concept(self, data: str) -> matrix:
+        """
+        Make a correlation matrix between the two documents.
+        """
+        tfidf = TfidfVectorizer()
+        concepts_tfidf = tfidf.fit_transform(self.concepts.names)
+        terms_tfidf = tfidf.transform(data)
+        return cosine_similarity(terms_tfidf, concepts_tfidf)
