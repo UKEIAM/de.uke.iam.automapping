7eca82a4126708c84d950a43e4a1287997f45718
Nusha34 <chechulinaan17@gmail.com>
2022-07-01 11:29:00 +0000

    new class for data representation

---
+++ src/automapping/view_for_results.py
@@ -0,0 +1,21 @@
+from mapper import Prediction
+from typing import Sequence
+import pandas as pd
+
+
+class View:
+    """
+    A view for the results of the automapping.
+    """
+
+    @staticmethod
+    def to_df(predictions: Sequence[Prediction]) -> pd.DataFrame:
+        """
+        Get a dataframe with the predictions.
+        """
+        df_with_results = pd.DataFrame()
+        df_with_results["SourceName"] = predictions.source_names
+        df_with_results["ConceptName"] = predictions.concept_names
+        df_with_results["ConceptID"] = predictions.concept_ids
+        df_with_results["MatchScore"] = predictions.confidence_scores
+        return df_with_results
