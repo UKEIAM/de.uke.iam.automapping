fc942b89a374bda14062a1201ced496be536789a
Nusha34 <chechulinaan17@gmail.com>
2022-07-06 13:19:31 +0000

    change code style

--- src/automapping/mapper.py
+++ src/automapping/mapper.py
@@ -13,10 +13,10 @@ class Prediction:
     A prediction with associated confidence score.
     """
 
-    source_names: str
-    concept_ids: int
-    concept_names: str
-    confidence_scores: float
+    source_name: str
+    concept_id: int
+    concept_name: str
+    confidence_score: float
 
 
 class Mapper:
@@ -41,37 +41,29 @@ class TfIdf(Mapper):
 
     def __init__(self, concepts: Sequence[Concept]):
         self.concepts = concepts
+        self.tfidf = TfidfVectorizer()
+        self.concepts_tfidf = self.tfidf.fit_transform(self.concepts.names)
 
     def __call__(self, data: str, num_guesses: int) -> Sequence[Prediction]:
         matrix_with_similarity_score = (
-            self.create_matrix_with_cosine_sim_between_term_concept(data)
+            self._create_matrix_with_cosine_sim_between_term_concept(data)
         )
-        source_names = []
-        concept_ids = []
-        concept_names = []
-        confidence_score = []
         for i in range(matrix_with_similarity_score.shape[0]):
             for seq_number, score in heapq.nlargest(
                 num_guesses,
                 enumerate(matrix_with_similarity_score[i]),
                 key=lambda x: x[1],
             ):
-                source_names.append(data[i])
-                concept_ids.append(self.concepts.concept_id[seq_number])
-                concept_names.append(self.concepts.names[seq_number])
-                confidence_score.append(score)
-        return Prediction(
-            source_names,
-            concept_ids,
-            concept_names,
-            confidence_score,
-        )
+                yield Prediction(
+                    data[i],
+                    self.concepts.concept_id[seq_number],
+                    self.concepts.names[seq_number],
+                    score,
+                )
 
-    def create_matrix_with_cosine_sim_between_term_concept(self, data: str) -> matrix:
+    def _create_matrix_with_cosine_sim_between_term_concept(self, data: str) -> matrix:
         """
         Method for creating a matrix with cosine similarity between term and concept.
         """
-        tfidf = TfidfVectorizer()
-        concepts_tfidf = tfidf.fit_transform(self.concepts.names)
-        source_tfidf = tfidf.transform(data)
-        return cosine_similarity(source_tfidf, concepts_tfidf)
+        source_tfidf = self.tfidf.transform(data)
+        return cosine_similarity(source_tfidf, self.concepts_tfidf)
