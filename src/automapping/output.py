c4518cd0b802b2acd1f5db6b3fff96ea20818d1e
Nusha34 <chechulinaan17@gmail.com>
2022-07-06 17:23:28 +0000

    without sequence

--- src/automapping/output.py
+++ src/automapping/output.py
@@ -1,21 +1,48 @@
 from .mapper import Prediction
-from typing import Sequence
 import pandas as pd
 
 
-class View:
+class Predictions:
     """
     A view for the results of the automapping.
     """
 
-    @staticmethod
-    def to_df(predictions: Sequence[Prediction]) -> pd.DataFrame:
+    def __init__(self, generator):
+        self.source_names = []
+        self.concept_names = []
+        self.concept_ids = []
+        self.confidence_scores = []
+
+        for item in generator:
+            self.source_names.append(item.source_name)
+            self.concept_names.append(item.concept_name)
+            self.concept_ids.append(item.concept_id)
+            self.confidence_scores.append(item.confidence_score)
+
+    def __len__(self):
+        return len(self.source_names)
+
+    def __getitem__(self, key):
+        return Prediction(
+            source_name=self.source_names[key],
+            concept_id=self.concept_ids[key],
+            concept_name=self.concept_names[key],
+            confidence_score=self.confidence_scores[key],
+        ).__new__()
+
+    def __setitem__(self, key: int, value: Prediction):
+        self.source_names[key] = value.source_name
+        self.concept_names[key] = value.concept_name
+        self.concept_ids[key] = value.concept_id
+        self.confidence_scores[key] = value.confidence_score
+
+    def to_df(self) -> pd.DataFrame:
         """
         Get a dataframe with the predictions.
         """
         df_with_results = pd.DataFrame()
-        df_with_results["SourceName"] = predictions.source_names
-        df_with_results["ConceptName"] = predictions.concept_names
-        df_with_results["ConceptID"] = predictions.concept_ids
-        df_with_results["MatchScore"] = predictions.confidence_scores
+        df_with_results["SourceName"] = self.source_names
+        df_with_results["ConceptName"] = self.concept_names
+        df_with_results["ConceptID"] = self.concept_ids
+        df_with_results["MatchScore"] = self.confidence_scores
         return df_with_results
