078ac6eceb17e1f01f548a45e65fb4df55a3153a
Nusha34 <chechulinaan17@gmail.com>
2022-07-11 21:15:01 +0000

    added sequence, map

--- src/automapping/detections.py
+++ src/automapping/detections.py
@@ -1,48 +1,41 @@
+from dataclasses import dataclass
 from .prediction import Prediction
+from typing import Sequence
 import pandas as pd
 
 
+@dataclass
 class Predictions:
     """
     A view for the results of the automapping.
     """
 
-    def __init__(self, generator):
-        self.source_names = []
-        self.concept_names = []
-        self.concept_ids = []
-        self.confidence_scores = []
-
-        for item in generator:
-            self.source_names.append(item.source_name)
-            self.concept_names.append(item.concept_name)
-            self.concept_ids.append(item.concept_id)
-            self.confidence_scores.append(item.confidence_score)
+    _detections: Sequence[Prediction]
 
     def __len__(self):
-        return len(self.source_names)
+        return len(self._detections)
 
     def __getitem__(self, key):
-        return Prediction(
-            source_name=self.source_names[key],
-            concept_id=self.concept_ids[key],
-            concept_name=self.concept_names[key],
-            confidence_score=self.confidence_scores[key],
-        ).__new__()
+        return self._detections[key]
 
     def __setitem__(self, key: int, value: Prediction):
-        self.source_names[key] = value.source_name
-        self.concept_names[key] = value.concept_name
-        self.concept_ids[key] = value.concept_id
-        self.confidence_scores[key] = value.confidence_score
+        self._detections[key] = value
 
     def to_df(self) -> pd.DataFrame:
         """
         Get a dataframe with the predictions.
         """
         df_with_results = pd.DataFrame()
-        df_with_results["SourceName"] = self.source_names
-        df_with_results["ConceptName"] = self.concept_names
-        df_with_results["ConceptID"] = self.concept_ids
-        df_with_results["MatchScore"] = self.confidence_scores
+        df_with_results["SourceName"] = list(
+            map(lambda x: x.source_name, self._detections)
+        )
+        df_with_results["ConceptName"] = list(
+            map(lambda x: x.concept_name, self._detections)
+        )
+        df_with_results["ConceptID"] = list(
+            map(lambda x: x.concept_id, self._detections)
+        )
+        df_with_results["MatchScore"] = list(
+            map(lambda x: x.confidence_score, self._detections)
+        )
         return df_with_results
