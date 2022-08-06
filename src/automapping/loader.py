f82173d96f1eca34efab26aa1d85e267c7f4f453
Nusha34 <chechulinaan17@gmail.com>
2022-08-06 14:21:52 +0000

    sort inputs and sort sourcenames

--- src/automapping/loader.py
+++ src/automapping/loader.py
@@ -1,6 +1,8 @@
 from typing import Iterable
-import pandas as pd
+
 import numpy as np
+import pandas as pd
+
 from .language import Language
 
 
@@ -33,4 +35,4 @@ class ExcelLoader(Loader):
         self.data = self.data.replace([" ", ""], np.nan)
         self.data[self.column] = self.data[self.column].str.strip()
         self.data = self.data[self.column].dropna(axis=0)
-        return iter(self.data)
+        return iter(sorted(self.data))
