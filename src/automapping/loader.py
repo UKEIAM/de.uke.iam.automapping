0cc9a603a898a5c6735c0540e139ad1e66742e01
Nusha34 <chechulinaan17@gmail.com>
2022-08-09 12:33:21 +0000

    delete sorting

--- src/automapping/loader.py
+++ src/automapping/loader.py
@@ -35,4 +35,4 @@ class ExcelLoader(Loader):
         self.data = self.data.replace([" ", ""], np.nan)
         self.data[self.column] = self.data[self.column].str.strip()
         self.data = self.data[self.column].dropna(axis=0)
-        return iter(sorted(self.data))
+        return iter(self.data)
