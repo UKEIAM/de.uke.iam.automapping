e3194acabf9736d7cb321b941f243b5eb03b6472
Nusha34 <chechulinaan17@gmail.com>
2022-12-14 14:17:51 +0100

    code review

--- src/automapping/m5_pipeline.py
+++ src/automapping/m5_pipeline.py
@@ -17,35 +17,39 @@ class M5:
         self.table = table
         self.url = f"http://{self.host}/m5.rest/api/{self.data_dictionary}/{self.version}/{self.table}"
 
-    def loader(self, language: Language) -> Iterable[str]:
+    def loader(self, source_language: Language) -> Iterable[str]:
         """
         Load data from M5
         """
         data_json = requests.Session().get(url=self.url).json()
         for element in data_json["elements"]:
             for label in element["labels"]:
-                if label["language"] == language.value.upper():
+                if label["language"] == source_language.value.upper():
                     yield element["name"], label["value"]
 
     def translation_uploader(
-        self, names_of_elements: list, translated_values: list, language: Language
+        self, names_of_elements: list, translated_values: list, target_language: Language
     ):
         """
         Upload translated data to M5
         """
         header = {"Content-type": "application/json", "Accept": "application/json"}
+        successfull_list_of_elements=[]
         for i, element in enumerate(names_of_elements):
-            body = requests.get(self.url + "/" + element, timeout=10).json()
-            if language.value.upper() not in body["definitions"].keys():
+            full_url="/".join((self.url, element))
+            body = requests.get(full_url, timeout=10).json()
+            if target_language.value.upper() not in body["definitions"].keys():
                 body["labels"].append(
                     {
-                        "language": language.value.upper(),
+                        "language": target_language.value.upper(),
                         "value": translated_values[i],
                         "type": "PREFERED",
                     }
                 )
-                body["definitions"]["EN"] = translated_values[i]
+                body["definitions"][target_language.value.upper()] = translated_values[i]
             upd = requests.patch(
-                self.url + "/" + element, json=body, headers=header, timeout=10
+                full_url, json=body, headers=header, timeout=10
             )
-            print(upd)
+            if upd.status_code == 200:
+                successfull_list_of_elements.append(element) 
+        print(f'Upload translation for elements {successfull_list_of_elements} completed successfully')
