d12859bbfff60031f9f5b7be311ab498af717a54
Nusha34 <chechulinaan17@gmail.com>
2023-02-27 06:03:26 +0000

    synonyms_to_class

--- src/automapping/concept.py
+++ src/automapping/concept.py
@@ -1,5 +1,5 @@
 from dataclasses import dataclass
-from typing import Sequence
+from typing import Iterable
 
 import pandas as pd
 
@@ -10,11 +10,12 @@ class Concept:
     A concept in a medical ontology like SNOMED-CT.
     """
 
-    names: Sequence[str]
-    concept_id: Sequence[int]
-    concept_code: Sequence[int]
-    domain_ids: Sequence[str]
-    voc_version: Sequence[str]
+    name: str
+    concept_id: int
+    concept_code: int
+    domain_id: str
+    voc_version: str
+    concept_synonyms: str
 
     @staticmethod
     def concatenate_concept_with_their_synonyms(
@@ -22,7 +23,7 @@ class Concept:
         synonyms: pd.DataFrame,
         vocabulary_table: pd.DataFrame,
         vocabulary_id: str,
-    ) -> "Concept":
+    ) -> Iterable["Concept"]:
         """
         Method for concatenation concepts and their synonyms
         """
@@ -33,28 +34,24 @@ class Concept:
         concepts["concept_name"] = (
             concepts["concept_name"].replace({"X]": "X] "}, regex=True).str.lower()
         )
-        main_concept_ids = concepts["concept_id"].tolist()
-        main_concept_codes = concepts["concept_code"].tolist()
-        main_domain_ids = concepts["domain_id"].tolist()
-        main_concept_names = list(map(str, concepts["concept_name"]))
-        synonyms = synonyms[synonyms["concept_id"].isin(main_concept_ids)]
-        synonyms = synonyms.merge(concepts, on="concept_id", how="left")
-        synonyms_concept_ids = synonyms["concept_id"].tolist()
-        synonyms_concept_codes = synonyms["concept_code"].tolist()
-        synonyms_domain_ids = ["Synonym"] * synonyms.shape[0]
-        synonyms_concept_names = list(
-            map(str, synonyms["concept_synonym_name"].str.lower())
-        )
         voc_version = vocabulary_table[
             vocabulary_table["vocabulary_id"] == vocabulary_id
         ].iloc[0]["vocabulary_version"]
-        voc_version_list = [voc_version] * len(
-            main_concept_names + synonyms_concept_names
-        )
-        return Concept(
-            main_concept_names + synonyms_concept_names,
-            main_concept_ids + synonyms_concept_ids,
-            main_concept_codes + synonyms_concept_codes,
-            main_domain_ids + synonyms_domain_ids,
-            voc_version_list,
-        )
+        for _, concept in concepts.iterrows():
+            concept_name = concept["concept_name"]
+            concept_id = concept["concept_id"]
+            concept_domain = concept["domain_id"]
+            concept_code = concept["concept_code"]
+            concept_synonyms = " ".join(
+                synonyms[synonyms["concept_id"] == concept_id][
+                    "concept_synonym_name"
+                ].tolist()
+            )
+            yield Concept(
+                concept_name,
+                concept_id,
+                concept_code,
+                concept_domain,
+                voc_version,
+                concept_synonyms,
+            )
